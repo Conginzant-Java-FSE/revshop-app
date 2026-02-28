1fd63f38ebbeb3a948868d05e12bb2a3f38fd966
Harini-JP <harinijpandian@gmail.com>
2026-03-01 01:41:56 +0530

    Implemented Product List Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { RegisterComponent } from './components/register/register';
 import { LoginComponent } from './components/login/login';
 import { DashboardComponent } from './components/dashboard/dashboard';
 import { authGuard } from './guards/auth.guard';
+import { ProductListComponent } from './components/product-list/product-list';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'products', component: ProductListComponent },
   { path: '**', redirectTo: '' }
 ];

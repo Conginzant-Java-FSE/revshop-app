3115bb44992c4c5b2cc2bd9f2849fba3fba1b2b2
vishnu-yadav0 <vishyadavviva84479@gmail.com>
2026-03-03 13:09:31 +0530

    added Profile component

---
+++ src/app/services/user.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../models/api-response.model';
+
+export interface UserDTO {
+    userId: number;
+    name: string;
+    email: string;
+    phone: string;
+    age: number;
+    role: string;
+}
+
+export interface PasswordUpdateRequest {
+    oldPassword: string;
+    newPassword: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private apiUrl = '/api/users';
+
+    constructor(private http: HttpClient) { }
+
+    getUserById(id: number): Observable<ApiResponse<UserDTO>> {
+        return this.http.get<ApiResponse<UserDTO>>(`${this.apiUrl}/${id}`);
+    }
+
+    updateProfile(id: number, userData: UserDTO): Observable<ApiResponse<UserDTO>> {
+        return this.http.put<ApiResponse<UserDTO>>(`${this.apiUrl}/${id}/profile`, userData);
+    }
+
+    updatePassword(id: number, request: PasswordUpdateRequest): Observable<ApiResponse<void>> {
+        return this.http.put<ApiResponse<void>>(`${this.apiUrl}/${id}/password`, request);
+    }
+}
